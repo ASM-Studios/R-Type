71bd089a817b9ff5bad08640bf2d87a9220b03f7
Mael-RABOT <mael.rabot@epitech.eu>
2024-09-17 09:01:46 +0200

    feat(windows): init window
    Signed-off-by: Mael-RABOT <mael.rabot@epitech.eu>

---
+++ client/include/GuiException.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <exception>
+
+namespace GUI {
+    class GuiException : public std::exception {
+    private:
+        std::string _msg;
+
+    public:
+        GuiException(std::string const &msg) : _msg(msg) {}
+        virtual ~GuiException() throw() {}
+        virtual const char *what() const throw() { return _msg.c_str(); }
+    };
+
+    class guiActionException : public std::exception {
+    private:
+        std::string _msg;
+
+    public:
+        guiActionException(std::string const &msg) : _msg(msg) {}
+        virtual ~guiActionException() throw() {}
+        virtual const char *what() const throw() { return _msg.c_str(); }
+    };
+}
