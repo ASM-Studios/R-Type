160c4e87570a9f6679b7d7f4bd01f09fda1bc7d5
Yohann Mangenot <yohann.mangenot@epitech.eu>
2024-09-25 13:40:07 +0200

    evol(server): add config file
    Signed-off-by: Yohann Mangenot <yohann.mangenot@epitech.eu>

---
+++ server/include/Logger.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "Config.hpp"
+
+enum class LogLevel {
+    NONE = -1,
+    INFO = 0,
+    WARNING = 1,
+    ERROR = 2
+};
+
+class Logger {
+private:
+    static LogLevel getLevel(const std::string& level) {
+        if (level == "INFO") {
+            return LogLevel::INFO;
+        } else if (level == "WARNING") {
+            return LogLevel::WARNING;
+        } else if (level == "ERROR") {
+            return LogLevel::ERROR;
+        }
+        return LogLevel::NONE;
+    }
+
+public:
+    static void log(const LogLevel logLevel, const std::string& message) {
+        Config& config = Config::getInstance("client/config.json");
+        if (const std::string configLogLevel = config.get("logLevel").value_or("NONE"); logLevel <= getLevel(configLogLevel)) {
+            const auto now = std::chrono::system_clock::now();
+            const std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+            const std::tm* now_tm = std::localtime(&now_time);
+
+            std::ostringstream timeStream;
+            timeStream << std::put_time(now_tm, "%Y-%m-%d %H:%M:%S");
+
+            switch (logLevel) {
+                case LogLevel::INFO:
+                    std::cout << "\033[32m[INFO " << timeStream.str() << "] " << message << "\033[0m" << std::endl;
+                    break;
+                case LogLevel::WARNING:
+                    std::cout << "\033[33m[WARNING " << timeStream.str() << "] " << message << "\033[0m" << std::endl;
+                    break;
+                case LogLevel::ERROR:
+                    std::cerr << "\033[31m[ERROR " << timeStream.str() << "] " << message << "\033[0m" << std::endl;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+};
--- server/src/Core.cpp
+++ server/src/Core.cpp
@@ -2,6 +2,8 @@
 #include "../modules/network/include/QueryHandler.hpp"
 #include "../modules/network/include/socket/Server.hpp"
 #include "Logging.hpp"
+#include "Config.hpp"
+#include <cstdlib>
 
 void Core::_waitTPS() {
     double elapsed = this->_tpsClock.get();
@@ -12,15 +14,19 @@ void Core::_waitTPS() {
 }
 
 Core::Core() :
-    _tps(20),
-    _tickTime(1000 / _tps),
     _isRunning(true) {}
 
 void Core::init(const std::span<char *>& args [[maybe_unused]]) {
+    Config& config = Config::getInstance(DEFAULT_CONFIG);
+
+    this->_tps = std::atoi(config.get("tps").value_or("20").c_str());
+    _tickTime = 1000 / _tps;
+    this->_port = std::atoi(config.get("port").value_or("8080").c_str());
+    this->_hitboxes_config_file = config.get("hitboxes_config_file").value_or("");
 }
 
 int Core::run() {
-    network::socket::udp::Server server(8080);
+    network::socket::udp::Server server(_port);
 
     while (this->_isRunning) {
         if (server.availableRequest()) {
