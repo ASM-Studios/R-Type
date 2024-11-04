301de044ce13264146b98da973acada45c47bce7
Mathieu <mathieu.borel@epitech.eu>
2024-11-04 14:17:05 +0100

    fix(network): update network stability
    Signed-off-by: Mathieu <mathieu.borel@epitech.eu>

--- debug.sh
+++ debug.sh
@@ -5,6 +5,9 @@ BUILD_FOLDER=cmake-build-debug
 if [ ! -d $BUILD_FOLDER ]; then
     mkdir $BUILD_FOLDER
 fi
+if  command -v ctags >/dev/null; then
+    ctags -R server shared client
+fi
 cd $BUILD_FOLDER
 cmake --preset=debug ..
 cmake --build .
--- shared/network/src/socket/Client.cpp
+++ shared/network/src/socket/Client.cpp
@@ -5,16 +5,15 @@
 #include <boost/asio.hpp>
 #include <boost/asio/ip/address_v4.hpp>
 #include <cstdint>
+#include <functional>
 #include <iostream>
 #include <memory>
 
 namespace network {
-    Client::NotInitialized::NotInitialized() :
-        _message(std::format("UDP Client is not init")) {}
-
-    const char *Client::NotInitialized::NotInitialized::what() const noexcept {
-        return this->_message.c_str();
-    }
+    Client::Client() :
+        _udpIP(std::nullopt),
+        _udpPort(std::nullopt),
+        _tcpSocket(std::nullopt) {}
 
     Client::Client(std::string hostname, int tcpPort, int udpPort) :
         _udpIP(boost::asio::ip::address_v4::from_string(hostname)),
@@ -22,7 +21,7 @@ namespace network {
         _tcpSocket(socket::NetworkManager::getInstance().getContext()) {
 
         try {
-            this->_tcpSocket.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4::from_string(hostname), tcpPort));
+            this->_tcpSocket.value().connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4::from_string(hostname), tcpPort));
             Logger::log(LogLevel::INFO, "Connected");
         } catch (std::exception& e) {
             Logger::log(LogLevel::ERR, e.what());
@@ -35,14 +34,17 @@ namespace network {
         _tcpSocket(std::move(socket)) {}
 
     bool Client::operator==(const Client& other) const {
-        return this->_udpIP == other._udpIP && this->_udpPort == other._udpPort;
+        return this->_udpIP.value() == other._udpIP.value() && this->_udpPort.value() == other._udpPort.value();
     }
 
     void Client::read() {
+        if (!this->_tcpSocket.has_value()) {
+            return;
+        }
         auto *request = new RawRequest();                            // NOLINT
         auto *remoteEndpoint = new boost::asio::ip::udp::endpoint(); // NOLINT
 
-        this->_tcpSocket.async_receive(boost::asio::buffer(request, sizeof(RawRequest)), [this, request, remoteEndpoint](auto error, auto bytes) {
+        this->_tcpSocket->async_receive(boost::asio::buffer(request, sizeof(RawRequest)), [this, request, remoteEndpoint](auto error, auto bytes) {
             if (error) {
                 Logger::log(LogLevel::ERR, error.what());
                 return;
@@ -81,26 +83,25 @@ namespace network {
         return id;
     }
 
-    boost::asio::ip::address_v4 Client::getUdpIP() const {
-        if (!this->_udpIP.has_value()) {
-            throw NotInitialized();
-        }
-        return this->_udpIP.value();
+    std::optional<boost::asio::ip::address_v4> Client::getUdpIP() const {
+        return this->_udpIP;
     }
 
-    boost::asio::ip::port_type Client::getUdpPort() const {
-        if (!this->_udpPort.has_value()) {
-            throw NotInitialized();
-        }
-        return this->_udpPort.value();
+    std::optional<boost::asio::ip::port_type> Client::getUdpPort() const {
+        return this->_udpPort;
     }
 
-    boost::asio::ip::tcp::socket& Client::getSocket() {
-        return this->_tcpSocket;
+    std::optional<std::reference_wrapper<boost::asio::ip::tcp::socket>> Client::getSocket() {
+        if (this->_tcpSocket.has_value()) {
+            return std::ref(this->_tcpSocket.value());
+        }
+        return std::nullopt;
     }
 }
 
 std::ostream& operator<<(std::ostream& os, const network::Client& client) {
-    os << client.getUdpIP() << " - " << client.getUdpPort();
+    if (client.getUdpIP().has_value() && client.getUdpPort().has_value()) {
+        os << client.getUdpIP().value() << " - " << client.getUdpPort().value();
+    }
     return os;
 }
